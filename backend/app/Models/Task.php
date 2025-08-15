e124a22cf94fc0b5ff494bca1c6ca1599ae87aa1
KevinJp21 <kevinjp821@gmail.com>
2025-08-14 20:15:53 -0500

    feat: create tables models

---
+++ backend/app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $table = 'tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+        'finish_date',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+}
